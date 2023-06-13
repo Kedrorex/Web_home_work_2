e4ee41f706a8f2b436e3ac670a6214a3932f53c8
Kedrorex <belousovvk@bk.ru>
2023-06-13 15:01:53 +0300

    Inicilizacija

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function MyFilm() {
+    var movie = prompt("Введите ваш любимый фильм:");
+    if (movie == "Бэтмен") {
+      alert("Вы угадали!");
+    } else {
+      alert("Попробуйте еще раз");
+    }
+  }
+
+function Arragon(){
+  var arr = [5, 2, 8, 1, 4, 9, 3, 6, 7];
+  alert(arr);
+  arr.reverse();
+  alert(arr);
+}
